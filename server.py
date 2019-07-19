a2ebe7c4632de3313dd69803d6ee9bf3486d834e
RedVeil <44397805+RedVeil@users.noreply.github.com>
2019-07-19 17:09:39 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -8,48 +8,19 @@ import forecasting
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Secret Key"
 
-@app.route('/test')
+@app.route('/test', methods = ["GET","POST"])
 def test():
+    if request.method == "POST":
+        form = request.form
+        print(form)
+        print(form["test1"])
     return render_template("test.html")
 
-class ExpertForm(FlaskForm):
-    timeframe_buy = IntegerField("Kaufzeitraum", default=60)
-    timeframe_sell = IntegerField("Verkaufszeitraum", default=10)
-    start_year = IntegerField("Start Jahr", default=2008)
-    end_year = IntegerField("End Jahr", default=2018)
-    start_amount = IntegerField("Start Kapital", default=1000)
-    average_threshold = IntegerField(
-        "Threshold Hoher Durchschnitt", default=2)
-    average_strikes = IntegerField("Strikes", default=1)
-    median_threshold = IntegerField("Threshold Hoher Median", default=2)
-    median_strikes = IntegerField("Strikes", default=1)
-    bad_trades_threshold = IntegerField(
-        "Threshold Negativer Trades", default=10)
-    bad_trades_strikes = IntegerField("Strikes", default=2)
-    bad_trades2_threshold = IntegerField(
-        "Threshold2 Negativer Trades", default=20)
-    bad_trades2_strikes = IntegerField("Strikes", default=4)
-    severe_trades_threshold = IntegerField(
-        "Threshold Trades unter -10%", default=10)
-    severe_trades_strikes = IntegerField("Strikes", default=4)
-    great_trades_threshold = IntegerField(
-        "Threshold Trades ueber 10%", default=5)
-    great_trades_strikes = IntegerField("Strikes", default=1)
-    averages_multiplier = IntegerField("Multiplier Durschnitt", default=100)
-    medians_multiplier = IntegerField("Multiplier Median", default=100)
-    bad_trades_multiplier = IntegerField(
-        "Multiplier Negative Trades", default=100)
-    severe_trades_multiplier = IntegerField(
-        "Multiplier Trades unter -10%", default=100)
-    great_trades_multiplier = IntegerField(
-        "Multiplier Trades ueber 10%", default=100)
-    submit = SubmitField()
 
 @app.route('/', methods=["GET", "POST"])
 def full_controll():
-    form = ExpertForm()
     if request.method == "POST":
-        form_data = form.data
+        form_data = request.form
         backtest_breakdowns = companies_and_filter.webcall(
             form_data)
         graph_high = []
@@ -72,7 +43,7 @@ def full_controll():
         return render_template('results.html', labels=total_buy_dates, values_high=graph_high,
         values_medium=graph_average, values_low=graph_low ,
         total_trades=total_trades, breakdowns=breakdowns, timeframe_buy = form_data["timeframe_buy"], timeframe_sell= form_data["timeframe_sell"])
-    return render_template("landing.html", form=form)
+    return render_template("dashforge/template/index.html")
 
 class ForecastForm(FlaskForm):
     timeframe_buy = IntegerField("Kaufzeitraum", default=60)
